bec6ac44aa76ab57e3a827ebd9740e9411a1020a
AzeematRaji <azeematjumoke@gmail.com>
2025-08-06 11:14:47 +0000

    infra changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = ">= 3.0.0, < 5.0.0"
+  version = "5.19.0"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
@@ -17,29 +17,61 @@ module "vpc" {
   }
 }
 
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = "20.37.2"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.33"
 
   enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
-  general-purpose = {
-    desired_size   = 2
-    max_size       = 3
-    min_size       = 1
-    instance_types = ["t3.small"]
-    capacity_type  = "ON_DEMAND"
+    general = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
   }
-}
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   tags = {
     Name = "mongo-eks"
+  }
+}
+
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "${var.cluster_name}-ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+module "eks_blueprints_addons" {
+  source = "aws-ia/eks-blueprints-addons/aws"
+  version = "1.22.0" 
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  eks_addons = {
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
   }
 }
